af54ebcffb58e7b0ef670ce9d2957952749fb3e8
ZavarinArtem <zavarin.artem@gmail.com>
2022-10-26 12:55:01 +0300

    ufra_sale_report fix

--- ufra_sale_report/models/sale_report.py
+++ ufra_sale_report/models/sale_report.py
@@ -7,13 +7,14 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    picking_type_code = fields.Selection([('incoming', 'Receipt'), ('outgoing', 'Delivery'), ('internal', 'Internal Transfer')], 'Type of Operation')
+    picking_type_code = fields.Selection(
+        [('incoming', 'Receipt'), ('outgoing', 'Delivery'), ('internal', 'Internal Transfer')], 'Type of Operation')
     driver_id = fields.Many2one(comodel_name='res.partner',
                                 string="Driver")
     driver_secondary_id = fields.Many2one(comodel_name='res.partner',
                                           string="Secondary Driver")
-    number_of_adults = fields.Integer(string="Number of Adults")
-    number_of_children = fields.Integer(string="Number of Children")
+    number_of_adults = fields.Float(string="Number of Adults")
+    number_of_children = fields.Float(string="Number of Children")
 
     def _select_sale(self, fields=None):
         res = super(SaleReport, self)._select_sale(fields)
@@ -39,10 +40,17 @@ class SaleReport(models.Model):
         return """
         %s 
         , pos.picking_type_code as picking_type_code
-        , pos.number_of_adults as number_of_adults
-        , pos.number_of_children as number_of_children
+        , sum(pos.number_of_adults / lq.count) as number_of_adults
+        , sum(pos.number_of_children / lq.count) as number_of_children
         , pos.driver_id as driver_id
         , pos.driver_secondary_id as driver_secondary_id
+        """ % res
+
+    def _from_pos(self):
+        res = super(SaleReport, self)._from_pos()
+        return """
+        %s 
+                left join (SELECT l.order_id, cast(count(l.id) as float) as count FROM pos_order_line l GROUP BY l.order_id) lq on (pos.id=lq.order_id)
         """ % res
 
     def _group_by_pos(self):
@@ -50,8 +58,6 @@ class SaleReport(models.Model):
         return """
         %s
         , pos.picking_type_code
-        , pos.number_of_adults
-        , pos.number_of_children
         , pos.driver_id
         , pos.driver_secondary_id        
         """ % res
